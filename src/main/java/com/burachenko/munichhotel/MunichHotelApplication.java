289a9ddbd5d6c9e3cb661ce7fed001fe880b5ade
vladimirburacenko <vladimir.burachenko@gmail.com>
2018-12-19 08:17:14 +0300

    First commit

---
+++ src/main/java/com/burachenko/munichhotel/MunichHotelApplication.java
@@ -0,0 +1,13 @@
+package com.burachenko.munichhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunichHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunichHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/burachenko/munichhotel/MunichHotelApplicationTests.java
@@ -0,0 +1,17 @@
+package com.burachenko.munichhotel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MunichHotelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
