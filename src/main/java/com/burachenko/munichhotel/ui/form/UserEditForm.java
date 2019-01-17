992cfaddc21b38a5de3509164b29732e2a86a87e
burachenko <vladimir.burachenko@gmail.com>
2019-01-17 10:25:17 +0300

    User edit form is prepared.

--- src/main/java/com/burachenko/munichhotel/ui/form/UserEditForm.java
+++ src/main/java/com/burachenko/munichhotel/ui/form/UserEditForm.java
@@ -1,8 +1,10 @@
 package com.burachenko.munichhotel.ui.form;
 
 import com.burachenko.munichhotel.dto.UserDto;
+import com.burachenko.munichhotel.enumeration.UserBlocking;
 import com.burachenko.munichhotel.enumeration.UserGender;
 import com.vaadin.ui.DateField;
+import com.vaadin.ui.NativeSelect;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.RadioButtonGroup;
 import com.vaadin.ui.TextField;
@@ -17,13 +19,21 @@ public class UserEditForm extends AbstractEditForm<UserDto>{
     private DateField birthdayField = new DateField("Birthday");
 
     private RadioButtonGroup<UserGender> checkGender = new RadioButtonGroup<>("Gender");
+    private NativeSelect<UserBlocking> selectBlocking = new NativeSelect<>("Blocking");
 
+    private TextField discountValueField = new TextField("Discount");
 
     public UserEditForm(final UserDto userDto) {
         super(userDto);
         checkGender.setItems(UserGender.values());
         checkGender.setSelectedItem(UserGender.M);
-        addComponents(emailField, passwordField, nameField, surnameField, telNumField, birthdayField, checkGender);
+
+
+        selectBlocking.setItems(UserBlocking.values());
+        selectBlocking.setEmptySelectionAllowed(false);
+        selectBlocking.setSelectedItem(UserBlocking.NONE);
+
+        addComponents(emailField, passwordField, nameField, surnameField, telNumField, birthdayField, checkGender, selectBlocking, discountValueField);
     }
 
     @Override
